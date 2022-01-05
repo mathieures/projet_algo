b956379b006b0385f382c4f8e2e99aa37836062a
mathieures <mathieuresseguier@gmail.com>
2022-01-05 20:52:55 +0100

    Simplifié remove_b_tags() et remove_tags() grâce aux expressions régulières

--- script_parsing.py
+++ script_parsing.py
@@ -5,29 +5,15 @@ import re
 
 def remove_b_tags(string):
     """
-        Fonction qui enlève les balise <b> ainsi que leur contenue
+        Fonction qui enlève les balises <b> ainsi que leur contenu
     """
-    startString = "<b>"
-    endString = "</b>"
-    start = string.find(startString)
-    end = string.find(endString)
-    # On vérifie pas que </b> existe bien car on part du principe que la chaine est
-    # bien formée (càd que toute balise <b> est fermée par une balise </b>)
-    while start != -1:
-        string = string[:start] + string[end+len(endString):]
-        start = string.find(startString)
-        end = string.find(endString)
-
+    string = re.sub("<b>.*?</b>", "", string)
     return string
 
 
 def remove_tags(string):
     """Enlève les balises de la chaîne passée en paramètre"""
-    if "<" in string:
-        start = string.index("<")
-        end = string.index(">") + 1
-        string = string[:start] + string[end:]
-        return remove_tags(string)
+    string = re.sub("<.+?>|</.+?>", "", string)
     return string
 
 
@@ -45,13 +31,12 @@ def parse_script(script_str):
 
 def main():
     script_str = "<b><i>Hello</i></b>, mister <i>villain</i>.\n<b>Welcome to my <i>kingdom</i>, my kingdom come.</b>"
-    # print(remove_b_tags(script_str))
-    # print(remove_tags(script_str))
-    # print(parse_script(script_str))
+    print(f"Base string: {script_str}\n")
     string = remove_b_tags(script_str)
+    print(f"After b tags removal : {string}")
     string = remove_tags(string)
-    print(f"String de base: {script_str}", end="\n\n")
-    print(f"Dico des mots: {parse_script(string)}")
+    print(f"After all tags removal : {string}")
+    print(f"Occurrences: {parse_script(string)}")
 
 
 if __name__ == '__main__':
