2a258c218a06cacd4aabde463c44c718676bde74
thomas <thomassalcedo@hotmail.fr>
2022-01-06 19:22:19 +0100

    Optimisation de Graph

--- Graph.py
+++ Graph.py
@@ -8,72 +8,66 @@ import networkx as nx
 
 class Graph():
 
-    def __init__(self, dico={}, graph=None):
+    def __init__(self, dico=None, graph=None):
         """
             Le graphe peut soit être généré grâce a un dico 
         """
         self.__dico = dico
         self.__graph = graph
         if graph is None:
-            nodes, edges = self.__create_nodes_and_edges()
-            G = nx.Graph()
-            G.add_nodes_from(nodes)
-            G.add_weighted_edges_from(edges)
-            self.__graph = G
-            self.__edges = list(G)
-            self.__nodes = G.edges()
+            if dico != None:
+                edges = self.__create_nodes_and_edges()
+                G = nx.Graph()
+                G.add_weighted_edges_from(edges)
+                del edges
+                self.__graph = G
 
     def __create_nodes_and_edges(self):
         """
             Methode privée
             Methode qui créé la liste des sommets et des arêtes puis les renvoies sous forme de tuple
         """
-        nodes = list(set(self.__dico.keys()))
+        # nodes = list(set(self.__dico.keys()))
         # Une liste de tuple de la forme [(sommet1, sommet2, poids) , (...), ...]
         edges = []
-        # Sert a conserver uniquement les sommets de départ et d'arrivé des arêtes
-        edges_without_weight = []
         for word in self.__dico:
             for wordLinkedTo in self.__dico[word]:
-                if (word, wordLinkedTo) not in edges_without_weight:
-                    edges.append(
-                        (word, wordLinkedTo, self.__dico[word][wordLinkedTo]))
-                    edges_without_weight.append((word, wordLinkedTo))
-                else:
-                    index = edges_without_weight.index((word, wordLinkedTo))
-                    # On augmente le poids de 1
-                    edges[index][2] += 1
+                edges.append(
+                    (word, wordLinkedTo, self.__dico[word][wordLinkedTo]))
 
             # Pour libérer de la mémoire
-            del self.__dico[word]
+            # del self.__dico[word]
 
-        return (nodes, edges)
+        # return (nodes, edges)
+        return edges
 
     def getNodes(self):
-        return self.__nodes
+        return list(self.__graph)
 
     def getEdges(self):
-        return self.__edges
+        return self.__graph.edges()
+
+    def __draw(self):
+        """
+            Factorisation de code
+        """
+        pos = nx.spring_layout(self.__graph)
+        nx.draw(self.__graph, pos, with_labels=True, font_weight='bold')
+        edge_weight = nx.get_edge_attributes(self.__graph, 'weight')
+        nx.draw_networkx_edge_labels(
+            self.__graph, pos, edge_labels=edge_weight)
 
     def show(self):
         if self.__graph is not None:
-            pos = nx.spring_layout(self.__graph)
-            nx.draw(self.__graph, pos, with_labels=True, font_weight='bold')
-            edge_weight = nx.get_edge_attributes(self.__graph, 'weight')
-            nx.draw_networkx_edge_labels(
-                self.__graph, pos, edge_labels=edge_weight)
+            self.__draw()
             plt.show()
 
     def save_as_png(self):
         """
             Methode qui sauvegarde le graphe en png
         """
-        if self.__graph is None:
-            pos = nx.spring_layout(self.__graph)
-            nx.draw(self.__graph, pos, with_labels=True, font_weight='bold')
-            edge_weight = nx.get_edge_attributes(self.__graph, 'weight')
-            nx.draw_networkx_edge_labels(
-                self.__graph, pos, edge_labels=edge_weight)
+        if self.__graph is not None:
+            self.__draw()
             plt.savefig("graph.png")
 
     # -----------------------------Ces fonctionnalités nécessitent l'installation de graphviz-----------------------------
